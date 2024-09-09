847b5ab90590fbbd597f7a47270af4cc03b06f4c
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2024-09-08 23:52:48 -0700

    fix service createview to works in a modal

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -20,17 +22,29 @@ class ServicesIndexView(LoginRequiredMixin, FilterView):
     # ordering = ['name']
     # search_fields = ['name']
 
-class ServicesCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class ServicesCreateView(LoginRequiredMixin, CreateView):
     model = Services
-    template_name = 'services/create.html'
+    template_name = 'services/create2.html'
     form_class = ServiceForm
     success_url = reverse_lazy('services:index')
-    success_message = 'Service was created successfully'
 
     def get_context_data(self, **kwargs) :
         context = super().get_context_data(**kwargs)
         context['title'] = 'Create Service'
         return context
+    
+    def post(self, request: HttpRequest, *args: str, **kwargs):
+        self.object = None
+        form = self.get_form()
+
+        if not form.is_valid():
+            context = self.get_context_data()
+            context['form'] = form
+            return render(request, self.template_name, context)
+        
+        self.object = form.save()
+        # messages.success(request, 'Service was created successfully')
+        return HttpResponse(200)
 
 class ServicesUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Services
